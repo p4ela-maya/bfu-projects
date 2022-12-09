91b7d6d3f98b178648121d45e436f9f71e458871
Makhfirat Arabova <MaShArabova@stud.kantiana.ru>
2022-12-09 10:26:57 +0200

    add lab6

---
+++ lab6/func.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+void read(int mas[][100], int n);
+
+void write(int mas[][100], int n);
+
+bool equal(int mas1[], int mas2[], int n);
+
+bool compare(int mas[][100], int n);
+
+bool ifprime(int n);
+
+int prmax(int mas[][100], int n);
+
+void minpr(int mas[][100], int j, int n, int max);
+
+void change(int mas[][100], int n);
+
+void swap(int& a, int& b);
+
+void swap(int a, int b, int& c, int& d);
